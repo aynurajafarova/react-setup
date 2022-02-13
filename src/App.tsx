9da2bf414657fb676a16cd5f6b670cd021163bc3
aynurajafarova <aynurafj@code.edu.az>
2022-02-13 15:36:41 +0400

    chore(images): add configuration for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
 
+import reactLogo from "./assets/img/tech_react.png";
+
 import "./assets/styles/main.scss";
 
 const App: FC = () => {
-  return <h1>React Typescript Webpack Starter Template</h1>;
+  return (
+    <>
+      <h1>React Typescript Webpack Starter Template</h1>
+      <img src={reactLogo} alt="react-logo" width={200} height={200} />
+    </>
+  );
 };
 
 export default App;
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   output: {
