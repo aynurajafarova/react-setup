fdd06acef5c08a9971e7ae6a113ee93d50d611ca
aynurajafarova <aynurafj@code.edu.az>
2022-02-13 15:43:14 +0400

    chore(svg): add configuration for svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 
 import reactLogo from "./assets/img/tech_react.png";
+import logo from "./assets/img/react-logo.svg";
 
 import "./assets/styles/main.scss";
 
@@ -9,6 +10,7 @@ const App: FC = () => {
     <>
       <h1>React Typescript Webpack Starter Template</h1>
       <img src={reactLogo} alt="react-logo" width={200} height={200} />
+      <img src={logo} alt="react-logo" width={200} height={200} />
     </>
   );
 };
