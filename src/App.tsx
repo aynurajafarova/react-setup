b14b568f08ee6ef9f7c20a26cb83a9dda903b96b
aynurajafarova <aynurafj@code.edu.az>
2022-02-13 16:30:20 +0400

    chore(package.json): change build script

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import "./assets/styles/main.scss";
 const App: FC = () => {
   return (
     <>
-      <h1>React Typescript Webpack Starter Template</h1>
+      <h1>React Typescript Webpack Starter Template {process.env.NODE_ENV}</h1>
       <img src={reactLogo} alt="react-logo" width={200} height={200} />
       <img src={logo} alt="react-logo" width={200} height={200} />
     </>
